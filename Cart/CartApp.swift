f6c62abea845f9e73a4b3de33d1f402bb02ab3e3
rick2785 <rjhrabowskie@gmail.com>
2020-10-27 17:53:57 -0500

    Initial Commit

---
+++ Cart/CartApp.swift
@@ -0,0 +1,17 @@
+//
+//  CartApp.swift
+//  Cart
+//
+//  Created by RJ Hrabowskie on 10/27/20.
+//
+
+import SwiftUI
+
+@main
+struct CartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cart
+//
+//  Created by RJ Hrabowskie on 10/27/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
